549e1e5194ecd36490c21f3e17a35ff4a963d5ce
Anjali <veranjali2503@gmail.com>
2025-06-30 21:33:48 +0530

    Adding splashscreen

---
+++ app/src/main/java/com/toonandtools/quoted/splashscreen/SplashScreenFragment.kt
@@ -0,0 +1,43 @@
+package com.toonandtools.quoted.splashscreen
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import com.toonandtools.quoted.R
+import com.toonandtools.quoted.databinding.FragmentSplashScreenBinding
+
+
+class SplashScreenFragment : Fragment() {
+   private var _binding: FragmentSplashScreenBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentSplashScreenBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val fadeIn = AnimationUtils.loadAnimation(requireContext(), R.anim.fade_in)
+        val scaleUp = AnimationUtils.loadAnimation(requireContext(), R.anim.scale_up)
+
+        binding.logoImage.startAnimation(scaleUp)
+        binding.textView.startAnimation(fadeIn) // Make sure the ID in your XML is correct
+
+        // Optional: move to next screen after a delay
+        Handler(Looper.getMainLooper()).postDelayed({
+            // Example: navigate to next fragment
+            // findNavController().navigate(R.id.action_splash_to_home)
+        }, 2000)
+    }
+
+
+}
